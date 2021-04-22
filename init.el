728b5373a06f8931a054262c96d5f6aca73373fc
Chaitanya Kapoor <chaitanya.7.kapoor@gmail.com>
2021-04-22 21:28:04 +0530

    add jupyter and ipynb config

--- init.el
+++ init.el
@@ -55,6 +55,18 @@
   :init
   (elpy-enable))
 
+;; Use IPython for REPL
+(setq python-shell-interpreter "jupyter"
+      python-shell-interpreter-args "console --simple-prompt"
+      python-shell-prompt-detect-failure-warning nil)
+(add-to-list 'python-shell-completion-native-disabled-interpreters
+             "jupyter")
+
+;; Enable Flycheck
+(when (require 'flycheck nil t)
+  (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
+  (add-hook 'elpy-mode-hook 'flycheck-mode))
+
 ;;Magit integration
 (use-package magit
   :ensure t
